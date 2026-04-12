2ad2a952513f6193b72a697bdef86924d81b6d72
RizuMong <rizkihaddiprayoga@gmail.com>
2026-04-12 11:33:57 +0700

    update: update approvers

--- src/request-time-off/timeoff-approver.config.js
+++ src/request-time-off/timeoff-approver.config.js
@@ -2,13 +2,13 @@ const TIMEOFF_APPROVERS = [
     {
         employee_id: 1,
         approver_name: "Manager",
-        email: "rizkiyoga177@gmail.com",
+        email: "fatkurfaris12@gmail.com",
         role: "MANAGER"
     },
     {
         employee_id: 2,
         approver_name: "HR",
-        email: "rizkihaddiprayoga@gmail.com",
+        email: "fatkurrizal12@gmail.com",
         role: "HR"
     }
 ];
